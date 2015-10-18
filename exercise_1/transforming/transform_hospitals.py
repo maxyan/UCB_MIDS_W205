74819141df1f9c955cfe3b915018f862450ceeaa
maxyan <max.yansu@gmail.com>
2015-10-18 12:30:51 +1100

    Update transform_hospitals
    Removing redundant logic

--- exercise_1/transforming/transform_hospitals.py
+++ exercise_1/transforming/transform_hospitals.py
@@ -11,11 +11,4 @@ hospitals = parts.map(lambda p:str(p[0])[1:-1]+','+str(p[1])[1:-1]+','+str(p[4])
 hospitals.saveAsTextFile('file:///data/exercise1/hospitals')
 
 # Name or address sometimes contains ',', this is an example. Think about how to correct
-seward = lines.filter(lambda x:"SEWARD" in x)
-
-# Below deals with creating dataframes and tables - below is no longer relevant
-# data_types = [StringType(), StringType(), StringType(), StringType(), StringType()]
-# fields = [StructField(field_name, StringType(), True) for field_name in schema_string.split()]
-# schema = StructType(fields)
-# hospitals = parts.map(lambda p:(p[0],p[1], p[3],p[4],p[5]))
-# schema_hospitals = sqlContext.createDataFrame(hospitals, schema)
+# seward = lines.filter(lambda x:"SEWARD" in x)
